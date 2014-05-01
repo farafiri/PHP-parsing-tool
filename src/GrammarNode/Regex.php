4c133eacb9da08e21113b9ab21dedd22f6c5f2ba
farafiri <fabianskirafal@gmail.com>
2014-05-01 09:10:30 +0200

    Removing route optimisation 3

--- src/GrammarNode/Regex.php
+++ src/GrammarNode/Regex.php
@@ -12,53 +12,6 @@ class Regex extends \ParserGenerator\GrammarNode\BaseNode Implements \ParserGene
     protected $eatWhiteChars;
     protected $caseInsensitive;
 
-    public function startChars()
-    {
-        switch ($this->givenRegex) {
-            case '/if\s*\(/':
-                return array('i' => true);
-            case '/foreach\s*\(/':
-                return array('f' => true);
-            case '/for\s*\(/':
-                return array('f' => true);
-            case '/while\s*\(/':
-                return array('w' => true);
-            case '/(return|break|continue|throw|include|include_once|require|require_once|echo)\s+/':
-            case '/(return|break|continue)(\s*;)?/':
-                return array('r' => true, 'b' => true, 'c' => true, 't' => true, 'i' => true, 'e' => true);
-            case '/(protected|public|private|static)\s+/':
-                return array('p' => true, 's' => true);
-            case '/\(\s*\)/':
-            case '/\((array|object|integer|int|boolean|bool|b|binary|string|real|float|double)\)/':
-                return array('(' => true);
-            case '/array\s*\(/':
-                return array('a' => true);
-            case '/:/':
-            case '/::/' :
-                return array(':' => true);
-            case '/[A-Za-z_][A-Za-z_0-9]*/':
-                $a = array('_' => true);
-                for ($i = ord('a'); $i <= ord('z'); $i++) {
-                    $a[chr($i)] = true;
-                }
-                for ($i = ord('A'); $i <= ord('Z'); $i++) {
-                    $a[chr($i)] = true;
-                }
-                return $a;
-            case '/\$[A-Za-z_][A-Za-z_0-9]*/':
-                return array('$' => true);
-            case '/-?\d+/':
-            case '/-?\d+(\.\d+)?/':
-                return array('-' => true, '0' => true, '1' => true, '2' => true, '3' => true, '4' => true, '5' => true, '6' => true, '7' => true, '8' => true, '9' => true);
-            case '/(true|false)/':
-                return array('t' => true, 'f' => true);
-            case '/(\/\*(.|\n)*\*\/|\/\/.*\n|)/':
-                return array('/' => true);
-            default:
-                return parent::startChars();
-        }
-    }
-
     public function __construct($regex, $eatWhiteChars = false, $caseInsensitive = false)
     {
         $this->eatWhiteChars = $eatWhiteChars;
